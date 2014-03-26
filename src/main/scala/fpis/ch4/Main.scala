041c5d6e133b25220df30048425c65625ebac399
Eugene Platonov <jozic@ua.fm>
2014-03-26 16:07:25 -0400

    ch 4, ex 3-4

--- src/main/scala/fpis/ch4/Main.scala
+++ src/main/scala/fpis/ch4/Main.scala
@@ -2,26 +2,37 @@ package fpis.ch4
 
 object Main extends App {
 
-  def opt[A](a: A): Option[A] = if (a == null) None else Some(a)
+  def some[A](a: A): Option[A] = Some(a)
 
+  def none[A]: Option[A] = None
 
-  assert(opt("1").map(_.toInt) == Some(1))
-  assert(opt(null: String).map(_.toInt) == None)
+  assert(some("1").map(_.toInt) == Some(1))
+  assert(none[String].map(_.toInt) == None)
 
-  assert(opt("1").flatMap(x => Some(x.toInt)) == Some(1))
-  assert(opt("1").flatMap(_ => None) == None)
-  assert(opt(null: String).flatMap(x => Some(x.toInt)) == None)
+  assert(some("1").flatMap(x => Some(x.toInt)) == Some(1))
+  assert(some("1").flatMap(_ => None) == None)
+  assert(none[String].flatMap(x => Some(x.toInt)) == None)
 
-  assert(opt("").getOrElse("a") == "")
-  assert(opt(null: String).getOrElse("a") == "a")
+  assert(some("").getOrElse("a") == "")
+  assert(none[String].getOrElse("a") == "a")
 
-  assert(opt("").orElse(Some("a")) == Some(""))
-  assert(opt(null: String).orElse(Some("a")) == Some("a"))
+  assert(some("").orElse(some("a")) == Some(""))
+  assert(none[String].orElse(some("a")) == Some("a"))
 
-  assert(opt("").filter(_.isEmpty) == Some(""))
-  assert(opt("q").filter(_.isEmpty) == None)
+  assert(some("").filter(_.isEmpty) == Some(""))
+  assert(some("q").filter(_.isEmpty) == None)
 
   assert(Options.mean(Seq(1, 2, 3, 4)) == Some(2.5))
   assert(Options.variance(Seq(1, 2, 3, 4)) == Some(1.25))
+
+  assert(Options.map2(some(1), some("2"))(_ + _.toInt) == Some(3))
+  assert(Options.map2(none[Int], Some("2"))(_ + _.toInt) == None)
+  assert(Options.map2(some(1), none[String])(_ + _.toInt) == None)
+
+
+  assert(Options.sequence(Nil) == Some(Nil))
+  assert(Options.sequence(List(some(1))) == Some(List(1)))
+  assert(Options.sequence(List(some(1), some(2), some(3))) == Some(List(1, 2, 3)))
+  assert(Options.sequence(List(some(1), some(2), some(3), none[Int])) == None)
 
 }
--- src/main/scala/fpis/ch4/Options.scala
+++ src/main/scala/fpis/ch4/Options.scala
@@ -12,4 +12,15 @@ object Options {
     m => mean(xs.map(x => math.pow(x - m, 2)))
   }
 
+  // ex 3
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    b.flatMap(bb => a.map(aa => f(aa, bb)))
+
+
+  // ex 4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a.foldRight(Some(List[A]()): Option[List[A]]) {
+    case (Some(aa), Some(l)) => Some(aa :: l)
+    case _ => None
+  }
+
 }
