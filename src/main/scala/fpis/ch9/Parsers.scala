a5966cd8382a637c0279cc905cca787f555e2395
Eugene Platonov <eplatonov@ebay.com>
2014-09-15 17:45:25 -0400

    ch 9, ex 5 (not yet)

--- src/main/scala/fpis/ch9/Parsers.scala
+++ src/main/scala/fpis/ch9/Parsers.scala
@@ -2,6 +2,8 @@ package fpis.ch9
 
 import fpis.ch8.{Gen, Prop}
 
+import scala.util.matching.Regex
+
 
 trait Parsers[ParseError, Parser[+ _]] {
   self =>
@@ -26,23 +28,32 @@ trait Parsers[ParseError, Parser[+ _]] {
 
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
-  def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
+  // primitives
 
   implicit def string(s: String): Parser[String]
 
-  def succeed[A](a: A): Parser[A] = string("").map(_ => a)
+  implicit def regex(r: Regex): Parser[String]
+
+  // ex 6
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+
+  def slice[A](p: Parser[A]): Parser[String]
 
   def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
 
   def product[A, B](p1: Parser[A], p2: Parser[B]): Parser[(A, B)]
 
+  //derived
+
+  def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
+
+  def succeed[A](a: A): Parser[A] = string("").map(_ => a)
+
   // ex 4
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
     if (n <= 0) succeed(List()) else map2(p, listOfN(n - 1, p))(_ :: _)
-
-  def zeroChars(c: Char): Parser[Int]
-
-  def zero[A, B](implicit f: A => B): Parser[B]
 
   def zeroOrMoreChars(c: Char): Parser[Int] = char(c).many.slice.map(_.size)
 
@@ -57,13 +68,14 @@ trait Parsers[ParseError, Parser[+ _]] {
   // ex 1
   def many1[A](p: Parser[A]): Parser[List[A]] = map2(p, p.many)(_ :: _)
 
-  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
-
-  def slice[A](p: Parser[A]): Parser[String]
-
   // ex 1
   def map2[A, B, C](p: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] =
-    (p ** p2).map { case (a, b) => f(a, b) }
+    (p ** p2).map { case (a, b) => f(a, b)}
+
+  def digitFollowedByChars(n: Int, c: Char): Parser[String] = regex("\\d".r).flatMap{ digit =>
+    listOfN(digit.toInt, char(c)).map(_.mkString)
+
+  }
 
   implicit def asParser[A, B](a: A)(implicit f: A => Parser[B]): ParserOps[B] = f(a)
 
@@ -81,6 +93,8 @@ trait Parsers[ParseError, Parser[+ _]] {
     def many1: Parser[List[A]] = self.many1(p)
 
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
 
     def map2[B, C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
 
